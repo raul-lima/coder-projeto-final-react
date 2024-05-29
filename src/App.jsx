837efd686fdaf1e41698466b1c15cc32f0e4a631
Raul Lima <raultorres.lima@gmail.com>
2024-05-28 21:01:43 -0300

    corrige alguns imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.scss";
 import { Navigation } from "./router/navigation/Navigation";
 import { Home } from "./router/home/Home";
 import { useState } from "react";
-import { Item } from "./router/item/Item";
+import { Item } from "./components/item/Item";
 
 export function App() {
   return (
--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -5,11 +5,9 @@ export function Item({ image, quantity }) {
     <div className="item-card">
       <img src={image} alt="Item" className="item-image" />
       <div className="item-actions">
-        <button onClick={onRemove} disabled={quantity === 0}>
-          -
-        </button>
+        <button>-</button>
         <span className="item-quantity">{quantity}</span>
-        <button onClick={onAdd}>+</button>
+        <button>+</button>
       </div>
     </div>
   );
